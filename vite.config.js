30173ccbacfd283e191ce1254067a0ee5a415aa4
jowi <jowimp15@gmail.com>
2024-08-26 17:24:48 -0300

    change configs of page.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//   },
+//   build: {
+//     outDir: 'dist',
+//     base: '/jowi_portafolio2/'
+//   }
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-  },
-  build: {
-    outDir: 'dist',
-    base: '/jowi_portafolio2/'
-  }
-})
+  base:"https://jowimp15.github.io/jowi_portafolio2/",
+});
